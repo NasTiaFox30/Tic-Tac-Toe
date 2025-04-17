61d1ab6540bbbbbfb12a9f64c2d846da01244ff4
Anastasiia Bzova <abzova1@stu.vistula.edu.pl>
2025-04-17 20:40:43 +0000

    Added private variables into class

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,14 @@
 //Imported classes
 import java.util.Random;
 import java.util.Scanner;
+
+public class TicTacToe {
+    private static char[][] board = new char[3][3];
+    private static final char PLAYER = 'X';
+    private static final char COMPUTER = 'O';
+    private static final char EMPTY = ' ';
+    private static Scanner scan = new Scanner(System.in);
+    private static Random random = new Random();
+    private static boolean restart = true;
+
+}
