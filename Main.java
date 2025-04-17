92e86c2303e3a7cb57b1a441932215ff6fd86cef
Anastasiia Bzova <abzova1@stu.vistula.edu.pl>
2025-04-17 20:50:57 +0000

    Refactor: added loop for restart game

--- Main.java
+++ Main.java
@@ -1,27 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        while (true) {
-            //1
-            TicTacToe.playerTurn();
+         while (TicTacToe.getRestart()) {
+            //Create empty table
+            TicTacToe.createBoard();
             TicTacToe.printBoard();
-            if (TicTacToe.isWin(TicTacToe.getPLAYER())) {
-                System.out.println("You WIN!");
-                break;
-            } else if (TicTacToe.isBoardFull()) {
-                System.out.println("Tie!");
-                break;
-            }
 
-            //2
-            TicTacToe.computerTurn();
-            TicTacToe.printBoard();
-            if (TicTacToe.isWin(TicTacToe.getComputer())) {
-                System.out.println("Computer WIN!");
-                break;
-            } else if (TicTacToe.isBoardFull()) {
-                System.out.println("Tie!");
-                break;
+            while (true) {
+                //1
+                TicTacToe.playerTurn();
+                TicTacToe.printBoard();
+                if (TicTacToe.isWin(TicTacToe.getPLAYER())) {
+                    System.out.println("You WIN!");
+                    break;
+                } else if (TicTacToe.isBoardFull()) {
+                    System.out.println("Tie!");
+                    break;
+                }
+
+                //2
+                TicTacToe.computerTurn();
+                TicTacToe.printBoard();
+                if (TicTacToe.isWin(TicTacToe.getComputer())) {
+                    System.out.println("Computer WIN!");
+                    break;
+                } else if (TicTacToe.isBoardFull()) {
+                    System.out.println("Tie!");
+                    break;
+                }
             }
+            
         }
     }
 }
