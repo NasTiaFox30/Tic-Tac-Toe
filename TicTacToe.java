2e1218cb5005f61ed76ef15421a0af7331fb3b73
Anastasiia Bzova <abzova1@stu.vistula.edu.pl>
2025-04-17 20:45:02 +0000

    Created: playerTurn() and computerTurn() + isValidMove

--- TicTacToe.java
+++ TicTacToe.java
@@ -41,5 +41,39 @@ public class TicTacToe {
             System.out.println("-------------");
         }
     }
+    
+    public static void playerTurn() {
+        int row, col;
+        do {
+            System.out.print("Write a row (1-3): ");
+            row = scan.nextInt() - 1;
+            System.out.print("Write a column (1-3): ");
+            col = scan.nextInt() - 1;
+        } while (!isValidMove(row, col));
+        board[row][col] = PLAYER;
+    }
+
+    private static boolean isValidMove(int row, int col) {
+        if (row < 0 || row >= 3 || col < 0 || col >= 3) {
+            System.out.println("Try again! Coordinates are wrong.");
+            return false;
+        }
+        if (board[row][col] != EMPTY) {
+            System.out.println("Try again! You can't do it here.");
+            return false;
+        }
+        return true;
+    }
+
+    //Random Computer move
+    public static void computerTurn() {
+        int row, col;
+        do {
+            row = random.nextInt(3);
+            col = random.nextInt(3);
+        } while (!isValidMove(row, col));
+        System.out.println("Комп'ютер зробив хід на " + (row + 1) + ", " + (col + 1));
+        board[row][col] = COMPUTER;
+    }
 }
 
