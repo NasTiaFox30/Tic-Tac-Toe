ab9510824ed983b0fb88d39c4448539846113079
Anastasiia Bzova <abzova1@stu.vistula.edu.pl>
2025-04-17 20:43:21 +0000

    Added new functions: printBoard() , createBoard()

--- TicTacToe.java
+++ TicTacToe.java
@@ -21,5 +21,25 @@ public class TicTacToe {
     public static boolean getRestart(){
         return restart;
     }
+    
+    public static void createBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = EMPTY;
+            }
+        }
+    }
+
+    public static void printBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
 }
 
